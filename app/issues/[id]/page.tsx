b5eb4963e48da77c24fd81d4706c7e1cfe2fda95
Mehryar <gargoyle.junior@gmail.com>
2025-07-18 18:49:01 +0330

    404 error bug fix

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -19,12 +19,14 @@ interface Props {
 export default async function IssueDetailPage({ params }: Props) {
   const param = await params;
   const issueId = parseInt(param.id);
+
+  //! If the given ID is not a number, return 404.
+  if (Number.isNaN(issueId)) notFound();
+
   const issue = await prisma.issue.findUnique({
     where: { id: issueId },
   });
 
-  //! If the given ID is not a number, return 404.
-  if (Number.isNaN(issueId)) notFound();
   //! If the given ID doesn't exist, return 404.
   if (!issue) notFound();
 
