3ac1e7e00a0864f4c3f6d5b07ae3be8d8a344ba5
Mehryar <gargoyle.junior@gmail.com>
2025-07-18 18:18:44 +0330

    Vercel troubleshoot #3

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,11 +1,3 @@
-/**
- * * We're using Radix UI components instead of writing TailwindCSS for consistency.
- * * We also had to install react-markdown in order to preview the Markdown content.
- * ! TailwindCSS has no default styling for H1 and ul/li tags, so we use the @tailwindcss/typography plugin to circumvent this.
- * ! Because of the newest TailwindCSS version, there is no tailwind.config.js file, we have to import the plugin for typography by adding @plugin "@tailwindcss/typography" to our global.css
- * ! for more info --> https://github.com/tailwindlabs/tailwindcss-typography
- */
-
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import { prisma } from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
@@ -18,13 +10,13 @@ interface Props {
 
 export default async function IssueDetailPage({ params }: Props) {
   const issueId = parseInt(params.id);
+
+  if (Number.isNaN(issueId)) notFound();
+
   const issue = await prisma.issue.findUnique({
     where: { id: issueId },
   });
 
-  //! If the given ID is not a number, return 404.
-  if (Number.isNaN(issueId)) notFound();
-  //! If the given ID doesn't exist, return 404.
   if (!issue) notFound();
 
   return (
