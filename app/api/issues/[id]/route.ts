155ea644cc0f8db25cc126c43494f4af02fc0d48
Mehryar <gargoyle.junior@gmail.com>
2025-07-22 22:22:16 +0330

    Vercel bug fix #1

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -2,14 +2,17 @@ import { prisma } from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { issueSchema } from "../validationSchema";
 
-export async function PATCH(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const param = await params;
+//! Vercel flagged the destructuring of params inside the PATCH parameters, so we'll have to extract dynamic route params from request.url instead.
+
+export async function PATCH(request: NextRequest) {
+  //! different from the course, due to updates.
+  const url = new URL(request.url);
+  const id = url.pathname.split("/").pop(); // Gets the [id] from the path
+  //! ---
+  const issueId = parseInt(id ?? "");
   const body = await request.json();
-  const issueId = parseInt(param.id);
   const validation = issueSchema.safeParse(body);
+
   if (!validation.success)
     return NextResponse.json(validation.error, { status: 400 });
   const issue = await prisma.issue.findUnique({ where: { id: issueId } });
