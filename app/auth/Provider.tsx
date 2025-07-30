07fee9a39de67d8aaaf4ff934dfceb0731151ce7
Mehryar <gargoyle.junior@gmail.com>
2025-07-30 20:16:33 +0330

    (Bug fix #3)

--- app/auth/Provider.tsx
+++ app/auth/Provider.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import React, { PropsWithChildren } from "react";
+import React, { isValidElement, PropsWithChildren } from "react";
 
 const AuthProvider = ({ children }: PropsWithChildren) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  console.log("AuthProvider children:", children);
+  console.log("Is valid React element?", isValidElement(children));
+  return <SessionProvider>{<>{children}</>}</SessionProvider>;
 };
 
 export default AuthProvider;
