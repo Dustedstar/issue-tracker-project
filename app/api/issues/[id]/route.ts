21f23c98e778b0733ffc4ff1cb4c15c0e106337e
Mehryar <gargoyle.junior@gmail.com>
2025-07-31 19:38:57 +0330

    Enhance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,6 +1,6 @@
 import { prisma } from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { issueSchema } from "../validationSchema";
+import { patchIssueSchema } from "../validationSchema";
 import authOptions from "@/app/auth/authOptions";
 import { getServerSession } from "next-auth";
 
@@ -13,9 +13,21 @@ export async function PATCH(request: NextRequest) {
   //! ---
   const issueId = parseInt(id ?? "");
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   const session = await getServerSession(authOptions);
   if (!session) return NextResponse.json({}, { status: 401 });
+
+  const { assignedToUserId, title, description } = body;
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+    if (!user)
+      return NextResponse.json(
+        { error: "User does not exist." },
+        { status: 400 }
+      );
+  }
 
   if (!validation.success)
     return NextResponse.json(validation.error, { status: 400 });
@@ -25,8 +37,9 @@ export async function PATCH(request: NextRequest) {
   const updatedIssue = await prisma.issue.update({
     where: { id: issueId },
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserId,
     },
   });
   return NextResponse.json(updatedIssue, { status: 200 });
