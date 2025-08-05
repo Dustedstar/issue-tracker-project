1988b806011572cafdc41fb00777d8de84d1aff7
Mehryar <gargoyle.junior@gmail.com>
2025-08-05 19:53:16 +0330

    (Bug fix: Dashboard)

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,3 @@
-import Pagination from "./components/Pagination";
-
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  const params = await searchParams;
-  return (
-    <Pagination
-      currentPage={parseInt(params.page)}
-      itemCount={100}
-      pageSize={10}
-    />
-  );
+export default async function Home() {
+  return <div>Hello World</div>;
 }
